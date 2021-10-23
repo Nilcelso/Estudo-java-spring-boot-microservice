66b24d34d6e69169eefaf100eb433161f82e528a
Nilcelso Sobrinho <nilcelsop@gmail.com>
2021-10-23 15:49:35 -0300

    Aplicação pronta

---
+++ src/main/java/com/microservice/email/MsEmailApplication.java
@@ -0,0 +1,16 @@
+package com.microservice.email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsEmailApplication.class, args);
+	}
+
+}
+
+
+//https://www.youtube.com/watch?v=ZBleZzJf6ro&list=RDCMUC2WbG8UgpPaLcFSNJYwtPow&start_radio=1&rv=ZBleZzJf6ro&t=154
